fcb5e7b175a00ecd47ba0aeed9f8547e36071961
Quang Nguyen <quangnguyen7616@gmail.com>
2023-03-26 00:32:19 +0700

    update

--- install.sh
+++ install.sh
@@ -88,7 +88,7 @@ WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
 mkdir $WORKDIR && cd $_
 
-IP4=$(curl -4 -s icanhazip.com)
+IP4="192.168.16.151"
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
