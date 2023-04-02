b9e073986cd4ddeaade34a9525e966fde0f5dee8
quangnguyen7616 <84000455+quangnguyen7616@users.noreply.github.com>
2023-04-03 03:09:46 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
-gen64() {
-	ip64() {
-		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-	}
-	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
-}
 IP4=$(curl -4 -s icanhazip.com)
-CC=gen64()
 echo "${IP4}"
-echo "${CC}"
+
