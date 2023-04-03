c66d4e9d0d99fd81ec967d7b5d223dd6b1235883
Quang Nguyen <quangnguyen7616@gmail.com>
2023-04-03 18:10:36 +0700

    update

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ install_3proxy() {
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-0.9.4
     make -f Makefile.Linux
-    #mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    #cp src/3proxy /usr/local/etc/3proxy/bin/
-    #cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-    #chmod +x /etc/init.d/3proxy
-    #chkconfig 3proxy on
+    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    cp bin/3proxy /usr/local/etc/3proxy/bin/
+    cp ./scripts/init.d/3proxy.sh /etc/init.d/3proxy
+    chmod +x /etc/init.d/3proxy
+    chkconfig 3proxy on
 }
 echo "installing apps"
 yum -y install wget gcc net-tools bsdtar zip >/dev/null
